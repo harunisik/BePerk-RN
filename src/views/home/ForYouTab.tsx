875232b1ab4210b7ca271f1b4a5e8c55611bff55
Harun Isik <harun1711@gmail.com>
2024-03-29 05:16:53 +0000

    4

--- src/views/home/ForYouTab.tsx
+++ src/views/home/ForYouTab.tsx
@@ -1,13 +1,10 @@
-import {Animated, Image, StyleSheet, Text, View} from 'react-native';
+import {Animated, View} from 'react-native';
 import common from '../../styles/sharedStyles';
 import PagerView from 'react-native-pager-view';
 import {getVideoFeed} from '../../services/UserService';
 import {useCustomQuery} from '../../hooks/commonHooks';
-import {SafeAreaView} from 'react-native';
-import {useEffect, useMemo, useState} from 'react';
-// import Video from '../../components/common/Video';
-import Video from 'react-native-video';
-import FastImage from 'react-native-fast-image';
+import {useState} from 'react';
+import Video from '../../components/common/Video';
 
 const {flex1, dashed} = common;
 
@@ -22,53 +19,25 @@ const ForYouTab = () => {
   });
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={flex1}>
       {data && (
         <AnimatedPagerView
-          // @ts-ignore
-          testID="pager-view"
-          style={styles.PagerView}
+          style={flex1}
           initialPage={0}
           layoutDirection="ltr"
-          pageMargin={10}
           orientation="vertical"
           onPageScroll={event => {
             setCurrentIndex(event.nativeEvent.position);
           }}>
           {data?.feed.map((page, index) => (
-            <View
-              testID="pager-view-content"
-              key={page.id}
-              style={{
-                alignItems: 'center',
-              }}
-              collapsable={false}>
-              <Video
-                source={{uri: page.filename}}
-                style={{width: '100%', height: '100%'}}
-                paused={index !== currentIndex}
-              />
+            <View key={page.id} collapsable={false}>
+              <Video uri={page.filename} paused={index !== currentIndex} />
             </View>
           ))}
         </AnimatedPagerView>
       )}
-    </SafeAreaView>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  image: {
-    width: 300,
-    height: 200,
-    padding: 20,
-  },
-  PagerView: {
-    flex: 1,
-  },
-});
 
 export default ForYouTab;
